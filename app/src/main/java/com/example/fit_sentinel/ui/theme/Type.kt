4d5bf653050fa46333c1435c7668cd6118cf96ad
menna <94606987+Menna120@users.noreply.github.com>
2025-04-23 16:33:10 +0200

    Syne fonts

--- app/src/main/java/com/example/fit_sentinel/ui/theme/Type.kt
+++ app/src/main/java/com/example/fit_sentinel/ui/theme/Type.kt
@@ -2,33 +2,60 @@ package com.example.fit_sentinel.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.fit_sentinel.R
 
-// Set of Material typography styles to start with
+val SyneFamily = FontFamily(
+    Font(R.font.syne_regular, FontWeight.Normal),
+    Font(R.font.syne_medium, FontWeight.Medium),
+    Font(R.font.syne_semi_bold, FontWeight.SemiBold),
+    Font(R.font.syne_bold, FontWeight.Bold)
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+    displayLarge = TextStyle(
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 50.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+
+    bodyMedium = TextStyle(
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontFamily = SyneFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 10.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.sp
     )
-    */
 )
