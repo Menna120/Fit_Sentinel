f3079e6fe6a83a88f92d87dcf1bd7939149fabfb
menna <94606987+Menna120@users.noreply.github.com>
2025-04-25 10:42:56 +0300

    begin reports

---
+++ app/src/main/java/com/example/fit_sentinel/ui/screens/main/reports/ReportsLayout.kt
@@ -0,0 +1,9 @@
+package com.example.fit_sentinel.ui.screens.main.reports
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ReportsLayout(modifier: Modifier = Modifier) {
+
+}
