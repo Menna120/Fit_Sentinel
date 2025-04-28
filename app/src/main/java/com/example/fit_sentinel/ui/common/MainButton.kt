43b262f30516a6c201f46e9e19aedf3f221ecfe3
menna <94606987+Menna120@users.noreply.github.com>
2025-04-28 22:45:18 +0300

    enhance MainButton composable

--- app/src/main/java/com/example/fit_sentinel/ui/common/MainButton.kt
+++ app/src/main/java/com/example/fit_sentinel/ui/common/MainButton.kt
@@ -1,11 +1,13 @@
 package com.example.fit_sentinel.ui.common
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fit_sentinel.R
 import com.example.fit_sentinel.ui.theme.Fit_SentinelTheme
@@ -23,26 +27,31 @@ import com.example.fit_sentinel.ui.theme.Fit_SentinelTheme
 fun MainButton(
     text: String,
     enabled: Boolean,
-    showIcon: Boolean,
     modifier: Modifier = Modifier,
+    showIcon: Boolean = false,
+    shape: Shape = CircleShape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        disabledContainerColor = MaterialTheme.colorScheme.secondary,
+        disabledContentColor = MaterialTheme.colorScheme.onPrimary
+    ),
+    border: BorderStroke? = null,
+    textStyle: TextStyle = MaterialTheme.typography.headlineMedium,
     onClick: () -> Unit
 ) {
     Button(
         onClick = onClick,
         modifier = modifier.fillMaxWidth(),
         enabled = enabled,
-        shape = CircleShape,
-        colors = ButtonDefaults.buttonColors(
-            disabledContainerColor = MaterialTheme.colorScheme.secondary,
-            disabledContentColor = MaterialTheme.colorScheme.onPrimary
-        )
+        shape = shape,
+        border = border,
+        colors = colors
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text, style = MaterialTheme.typography.headlineMedium)
+            Text(text, style = textStyle)
             if (showIcon) {
                 Spacer(modifier = Modifier.weight(1f))
                 Icon(
@@ -59,6 +68,6 @@ fun MainButton(
 @Composable
 private fun MainButtonPreview() {
     Fit_SentinelTheme {
-        MainButton("Let’s get started", true, true) {}
+        MainButton("Let’s get started", true) {}
     }
 }
