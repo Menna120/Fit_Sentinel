0e5ee642d61a54418e5af4f20653a762e7eb1133
menna <94606987+Menna120@users.noreply.github.com>
2025-04-30 22:03:54 +0300

    Implement start/stop step tracking toast messages

--- app/src/main/java/com/example/fit_sentinel/ui/screens/main/home/HomeScreen.kt
+++ app/src/main/java/com/example/fit_sentinel/ui/screens/main/home/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.fit_sentinel.ui.screens.main.home
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -11,8 +14,16 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
-
+    val context = LocalContext.current
     val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(uiState.isRecording) {
+        Toast.makeText(
+            context,
+            if (uiState.isRecording) "Session Started" else "Session Ended",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 
     HomeLayout(
         selectedDate = uiState.selectedDate,
