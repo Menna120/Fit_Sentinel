b8dd1d941d8a2cf0144446976fde78da1e398a07
menna <94606987+Menna120@users.noreply.github.com>
2025-05-01 11:25:08 +0300

    Animate play button and add icons

--- app/src/main/java/com/example/fit_sentinel/ui/screens/main/home/components/StepProgressDisplay.kt
+++ app/src/main/java/com/example/fit_sentinel/ui/screens/main/home/components/StepProgressDisplay.kt
@@ -1,25 +1,29 @@
 package com.example.fit_sentinel.ui.screens.main.home.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.fit_sentinel.R
 import com.example.fit_sentinel.ui.common.Arc
 import com.example.fit_sentinel.ui.theme.Black
 import com.example.fit_sentinel.ui.theme.Fit_SentinelTheme
@@ -31,12 +35,13 @@ fun StepProgressDisplay(
     onButtonClick: () -> Unit,
     modifier: Modifier = Modifier,
     labelText: String = "Steps",
-    buttonIcon: ImageVector = Icons.Default.PlayArrow,
     labelColor: Color = Black.copy(alpha = .5f),
     stepsColor: Color = Black,
     buttonBackgroundColor: Color = MaterialTheme.colorScheme.primary,
     buttonIconColor: Color = MaterialTheme.colorScheme.onPrimary
 ) {
+    var buttonState by remember { mutableStateOf(false) }
+
     Arc(steps, targetSteps, modifier) {
         Column(
             modifier = Modifier.fillMaxHeight(),
@@ -67,18 +72,25 @@ fun StepProgressDisplay(
             }
 
             Button(
-                onClick = onButtonClick,
+                onClick = {
+                    buttonState = !buttonState
+                    onButtonClick()
+                },
                 colors = ButtonDefaults.buttonColors(containerColor = buttonBackgroundColor),
                 modifier = Modifier.size(50.dp),
                 shape = CircleShape,
                 contentPadding = PaddingValues(0.dp)
             ) {
-                Icon(
-                    imageVector = buttonIcon,
-                    contentDescription = "Start/Stop",
-                    tint = buttonIconColor,
-                    modifier = Modifier.size(32.dp)
-                )
+                AnimatedContent(targetState = buttonState, label = "") { isPlaying ->
+                    Icon(
+                        painter = painterResource(if (isPlaying) R.drawable.equal else R.drawable.play),
+                        contentDescription = "Start/Stop",
+                        tint = buttonIconColor,
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                }
+
             }
         }
     }
