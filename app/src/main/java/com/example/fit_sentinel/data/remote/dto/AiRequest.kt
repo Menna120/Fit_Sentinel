20412a787af31441298c80bc7baedc1cdb062122
menna <94606987+Menna120@users.noreply.github.com>
2025-05-02 16:24:52 +0300

    replace serialname with direct name

--- app/src/main/java/com/example/fit_sentinel/data/remote/dto/AiRequest.kt
+++ app/src/main/java/com/example/fit_sentinel/data/remote/dto/AiRequest.kt
@@ -1,13 +1,12 @@
 package com.example.fit_sentinel.data.remote.dto
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AiRequest(
     val age: Int,
     val height: Int,
-    @SerialName("activity_lvl") val activityLevel: String,
-    @SerialName("body_goal") val bodyGoal: Float,
-    @SerialName("current_weight") val weight: Float,
+    val activity_lvl: String,
+    val body_goal: Float,
+    val current_weight: Float,
 )
